13a44a37c3d47f398047abcb0b4fe15451fef252
duhem-s <duhem-s@mgdis.fr>
2023-04-18 11:03:08 +0200

    test(notification-center): update screenshot after merge master
    CORE-2417

--- packages/notification-center/playwright.config.ts
+++ packages/notification-center/playwright.config.ts
@@ -14,7 +14,7 @@ const config: PlaywrightTestConfig = {
 	testMatch: '*.e2e.ts',
 	snapshotPathTemplate: '{testFileDir}/__screenshots__/{arg}-{projectName}{ext}',
 	/* Maximum time one test can run for. */
-	timeout: 30 * 1000,
+	timeout: 60 * 1000,
 	expect: {
 		/**
 		 * Maximum time expect() should wait for the condition to be met.
