d19b68bf3f990e87a6adec3f2e40a096c3b392a0
duhem-s <duhem-s@mgdis.fr>
2023-06-06 16:12:00 +0200

    chore: rename icon conf file

---
+++ packages/mg-components/src/components/atoms/mg-icon/mg-icon.conf.ts
@@ -0,0 +1,29 @@
+/**
+ * List of all possibles sizes
+ */
+export const sizes = ['small', 'regular', 'medium', 'large', 'extra-large'] as const;
+
+/**
+ * Icon Size from sizes
+ */
+export type IconSizeType = (typeof sizes)[number];
+
+/**
+ * List of all possibles variants
+ */
+export const variants = ['success', 'warning', 'danger', 'info', 'app'] as const;
+
+/**
+ * Variant type from variants
+ */
+export type IconVariantType = (typeof variants)[number];
+
+/**
+ * List of all possibles variants styles
+ */
+export const variantStyles = ['icon', 'background', 'full'] as const;
+
+/**
+ * Variant style type from variant style
+ */
+export type IconVariantStyleType = (typeof variantStyles)[number];
