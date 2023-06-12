ffb4469de96b963805bfc8759f48267d5e331839
jouanno-n <jouanno-n@mgdis.fr>
2023-06-12 19:36:32 +0200

    feat(typography): update small typo
    CORE-2419

--- packages/mg-components/src/test/typography.e2e.playwright.ts
+++ packages/mg-components/src/test/typography.e2e.playwright.ts
@@ -1,17 +1,17 @@
 import { setPageContent, expect, describe, testEach, PageType } from '../utils/playwright.e2e.test.utils';
 
 describe('typography', () => {
-  testEach([
-    '<h1>Heading 1</h1>',
-    '<h2>Heading 2</h2>',
-    '<h3>Heading 3</h3>',
-    '<h4>Heading 4</h4>',
-    '<h5>Heading 5</h5>',
-    '<h6>Heading 6</h6>',
-    '<p>paragraph</p>',
-    '<p><strong>strong</strong></p>',
-    '<p><small>small</small></p>',
-  ])('Should renders typography %s', async (page: PageType, html: string) => {
+  testEach(
+    ['1', '2', '3', '4', '5', '6'].flatMap(tag =>
+      [true, false].map(smallTag => (smallTag ? `<h${tag}><small>Heading ${tag} small</small></h${tag}>` : `<h${tag}>Heading ${tag}</h${tag}>`)),
+    ),
+  )('Should render heading %s', async (page: PageType, html: string) => {
+    await setPageContent(page, html);
+
+    await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+  });
+
+  testEach(['<p>paragraph</p>', '<p><strong>strong</strong></p>', '<p><small>small</small></p>'])('Should render paragraph %s', async (page: PageType, html: string) => {
     await setPageContent(page, html);
 
     await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
