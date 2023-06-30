939666e5f6ae7c43f1292255f56b6537049d10ec
duhem-s <duhem-s@mgdis.fr>
2023-06-30 11:33:33 +0200

    chore: apply prettier

--- packages/mg-components/src/stories/icon-gallery.tsx
+++ packages/mg-components/src/stories/icon-gallery.tsx
@@ -6,14 +6,11 @@ import React from 'react';
  * Template
  * @returns HTMLElement
  */
-const Template = (): HTMLElement => (
-   React.createElement(IconGallery, null,
-      iconList.map((icon) => (
-        React.createElement(IconItem, { name: icon, key: icon },
-          React.createElement('mg-icon', { icon: icon })
-        )
-      ))
-    )
-);
+const Template = (): HTMLElement =>
+  React.createElement(
+    IconGallery,
+    null,
+    iconList.map(icon => React.createElement(IconItem, { name: icon, key: icon }, React.createElement('mg-icon', { icon: icon }))),
+  );
 
 export default Template;
