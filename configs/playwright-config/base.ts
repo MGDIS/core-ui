50149b0575ef644920c89ce6fd1f8ca561fa96df
duhem-s <duhem-s@mgdis.fr>
2023-09-06 17:03:56 +0200

    chore: update ci
    CORE-2637

--- configs/playwright-config/base.ts
+++ configs/playwright-config/base.ts
@@ -31,7 +31,10 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never' }], ['junit', { outputFile: 'results.xml' }]],
+  reporter: [
+    ['html', { open: 'never' }],
+    ['junit', { outputFile: 'results.xml' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: true,
--- scripts/run-e2e.sh
+++ scripts/run-e2e.sh
@@ -44,7 +44,7 @@ prepare_package "packages/mg-components/package.json" '{
   "scripts": { 
     prebuild: .scripts.prebuild,
     start: .scripts.start,
-    "test:e2e:playwright": .scripts."test:e2e:playwright"
+    "test:e2e:playwright": .scripts."test:e2e:playwright:docker"
   }, 
   "dependencies": { "@stencil/core": .dependencies."@stencil/core" }, 
   "devDependencies": { "@stencil/sass": .devDependencies."@stencil/sass" }
@@ -58,14 +58,14 @@ prepare_package "packages/notification-center/package.json" '{
   "files": .files, 
   "exports": .exports, 
   "scripts": { 
-    "test:e2e:playwright": .scripts."test:e2e:playwright"
+    "test:e2e:playwright": .scripts."test:e2e:playwright:docker"
   }
 }'
 
 # Prepare packages/stylesheets/package.json
 prepare_package "packages/stylesheets/package.json" '{
   name, 
-  "scripts": { "test:e2e:playwright": .scripts."test:e2e:playwright" }
+  "scripts": { "test:e2e:playwright": .scripts."test:e2e:playwright:docker" }
 }'
 
 # Prepare apps/notification-center/package.json
