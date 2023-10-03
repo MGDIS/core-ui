aa1da2dffd66663bb79065d4be1631e36e7bde40
duhem-s <duhem-s@mgdis.fr>
2023-10-03 15:22:02 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- packages/mg-components/src/components/molecules/mg-popover/test/mg-popover.spec.tsx
+++ packages/mg-components/src/components/molecules/mg-popover/test/mg-popover.spec.tsx
@@ -156,7 +156,7 @@ describe('mg-popover', () => {
     expect(page.root).toMatchSnapshot();
   });
 
-  test('should update mg-popover-content id when identifier is update', async () => {
+  test('should update mg-popover-content id when identifier is updated', async () => {
     const page = await getPage({ identifier: 'identifier' }, [
       <h2 slot="title">Blu bli blo bla</h2>,
       <p slot="content">
