6b1ce3b71db02f53328d422fe271f5b7e9b7cb08
duhem-s <duhem-s@mgdis.fr>
2023-10-12 07:50:29 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- packages/mg-components/src/components/molecules/inputs/mg-input-textarea/mg-input-textarea.tsx
+++ packages/mg-components/src/components/molecules/inputs/mg-input-textarea/mg-input-textarea.tsx
@@ -132,7 +132,7 @@ export class MgInputTextarea {
   @Watch('patternErrorMessage')
   validatePattern(newValue: string): void {
     if (newValue !== undefined && !(isValidString(this.pattern) && isValidString(this.patternErrorMessage))) {
-      throw new Error('<mg-input-textarea> prop "pattern" and "patternErrorMessage" must be must be a non empty string and paired.');
+      throw new Error('<mg-input-textarea> props "pattern" and "patternErrorMessage" must be non-empty string and paired.');
     }
   }
 
