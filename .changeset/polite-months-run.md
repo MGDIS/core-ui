8e0171f4e1eeb9cbe2b0aacfff1ad7e9b81e9f8c
jouanno-n <jouanno-n@mgdis.fr>
2023-10-24 10:47:34 +0200

    fix(mgMenu): move mg-menu e2e tests to playwright to improve stability
    #398 CORE-2854

---
+++ .changeset/polite-months-run.md
@@ -0,0 +1,5 @@
+---
+'@mgdis/mg-components': patch
+---
+
+[#398](https://gitlab.mgdis.fr/core/core-ui/core-ui/-/issues/398) e2e: move mg-menu tests to playwright to improve stability
---
+++ packages/mg-components/src/components/molecules/menu/mg-menu-item/test/mg-menu-item.e2e.playwright.ts
@@ -0,0 +1,93 @@
+import { renderAttributes } from '../../../../../utils/e2e.test.utils';
+import { Direction, sizes } from '../../mg-menu/mg-menu.conf';
+import { Status } from '../mg-menu-item.conf';
+import { PageType, describe, describeEach, expect, setPageContent, test, testEach } from '../../../../../utils/playwright.e2e.test.utils';
+
+const TIMEOUT = 1000;
+const slotContent = '<div><h3>Demo title</h3><p>some content</p></div>';
+const slotMenuItem = '<mg-menu label="submenu"><mg-menu-item><span slot="label">Batman begins</span></mg-menu-item></mg-menu>';
+const slotImage = '<mg-icon icon="user" slot="image"></mg-icon>';
+const slotInformation = '<mg-badge value="2" label="hello" slot="information"></mg-badge>';
+const slotMetadata = '<span slot="metadata">is a hero</span>';
+
+const createHTML = (args, slot = '', direction = Direction.HORIZONTAL) => `
+<mg-menu ${renderAttributes({ label: 'batmenu', direction, ...args })}">
+  <mg-menu-item ${renderAttributes(args)}>
+    <span slot="label">${args.label ? args.label : 'batman'} ${args.href ? 'link' : ''}</span>
+    ${slot}
+  </mg-menu-item>
+</mg-menu>
+`;
+
+describe('mg-menu-item', () => {
+  describeEach([Direction.HORIZONTAL, Direction.VERTICAL])('render %s', (direction: Direction) => {
+    testEach(
+      [Status.ACTIVE, Status.VISIBLE, Status.HIDDEN, Status.DISABLED].flatMap(status =>
+        [undefined, '#link'].flatMap(href => [true, false].map(submenu => createHTML({ status, href }, submenu && slotMenuItem, direction))),
+      ),
+    )('should render whith status %s', async (page: PageType, html: string) => {
+      await setPageContent(page, html, { width: 100, height: 38 });
+
+      await page.locator('mg-menu-item.hydrated').first().waitFor({ timeout: TIMEOUT });
+
+      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+    });
+
+    testEach(
+      [
+        { label: 'submenu', slot: slotMenuItem },
+        { label: 'image', slot: slotImage },
+        { label: 'information', slot: slotInformation },
+        { label: 'information AND image', slot: slotInformation + slotImage },
+      ].flatMap(({ label, slot }) =>
+        [slotMenuItem, ''].flatMap(submenu =>
+          [slotMetadata, ''].map(metadata => createHTML({ label, size: metadata !== '' ? 'medium' : 'regular' }, [slot, metadata, submenu].join(''), direction)),
+        ),
+      ),
+    )('should render whith slots %s', async (page: PageType, html: string) => {
+      await setPageContent(page, html, { width: 130, height: 60 });
+
+      await page.locator('mg-menu-item.hydrated').first().waitFor({ timeout: TIMEOUT });
+
+      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+    });
+
+    testEach(sizes)(`should renders direction=${direction}, props size=%s`, async (page: PageType, size) => {
+      await setPageContent(page, createHTML({ size }, [slotInformation, slotImage, slotMenuItem].join(''), direction), { width: 130, height: 110 });
+
+      await page.locator('mg-menu-item.hydrated').first().waitFor({ timeout: TIMEOUT });
+
+      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+    });
+
+    testEach([true, false])(`should renders direction=${direction}, props expanded=%s`, async (page: PageType, expanded: boolean) => {
+      await setPageContent(page, createHTML({ expanded }, slotMenuItem, direction), { width: 130, height: 110 });
+
+      await page.locator('mg-menu-item.hydrated').first().waitFor({ timeout: TIMEOUT });
+
+      await expect(page.locator(expanded ? 'body' : '.e2e-screenshot')).toHaveScreenshot();
+    });
+
+    test('should render content slot', async ({ page }) => {
+      await setPageContent(page, createHTML({ expanded: true }, slotContent, direction), { width: 100, height: 150 });
+
+      await page.locator('mg-menu-item.hydrated').first().waitFor({ timeout: TIMEOUT });
+
+      await expect(page.locator('body')).toHaveScreenshot();
+    });
+
+    testEach([Status.ACTIVE, Status.VISIBLE, Status.HIDDEN, Status.DISABLED])('shoud manage keyboard navigation %s', async (page: PageType, status) => {
+      await setPageContent(page, createHTML({ status }, slotMenuItem, direction), { width: 120, height: 200 });
+
+      await page.locator('mg-menu-item.hydrated').first().waitFor({ timeout: TIMEOUT });
+
+      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+
+      for await (const key of ['Tab', 'Enter']) {
+        await page.keyboard.press(key);
+        await page.waitForTimeout(200);
+        await expect(page.locator('body')).toHaveScreenshot();
+      }
+    });
+  });
+});
