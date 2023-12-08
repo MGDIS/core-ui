18e9a03f0747fb4934d0bda5f7d00c5a209f7c68
duhem-s <duhem-s@mgdis.fr>
2023-12-08 11:34:34 +0100

    fix(notif-center): update playwright conf
    CORE-2945

--- packages/notification-center/playwright.config.ts
+++ packages/notification-center/playwright.config.ts
@@ -1,29 +1,12 @@
 import { config } from '@mgdis/playwright-helpers';
-import { devices, defineConfig } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   ...config,
-  /* Maximum time one test can run for. */
-  timeout: 60 * 1000,
-  /**
-   * Configure projects for major browsers
-   * For now we only test on Chromium
-   */
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
-  ],
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm prebuild && pnpm stencil build --serve --watch -p 3333',
-    url: 'http://localhost:3333',
-    reuseExistingServer: !(process.env.CI as boolean),
-  },
-  use: {
-    baseURL: 'http://localhost:3333',
+    command: 'cd ../.. && pnpm apps:notification-center',
+    port: 3210,
+    reuseExistingServer: true,
   },
 });
