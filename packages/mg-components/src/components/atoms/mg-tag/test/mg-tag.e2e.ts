b2d3f1a5a570f0732e4385e306c1e93ad9e3cadc
duhem-s <duhem-s@mgdis.fr>
2023-12-18 14:34:09 +0100

    chore(e2e): update mg-tag tests
    CORE-2967

--- packages/mg-components/src/components/atoms/mg-tag/test/mg-tag.e2e.ts
+++ packages/mg-components/src/components/atoms/mg-tag/test/mg-tag.e2e.ts
@@ -1,41 +1,43 @@
-import { darkBackground, renderAttributes } from '../../../../utils/e2e.test.utils';
-import { PageType, describe, expect, setPageContent, test, testEach } from '../../../../utils/playwright.e2e.test.utils';
+import { expect } from '@playwright/test';
+import { renderAttributes } from '@mgdis/playwright-helpers';
+import { test } from '../../../../utils/playwright.fixture';
 import { variants } from '../mg-tag.conf';
 
-describe('mg-tag', () => {
-  testEach(
-    variants.flatMap(variant =>
-      [true, false].flatMap(icon =>
-        [
-          { outline: false, soft: false },
-          { outline: true, soft: false },
-          { outline: false, soft: true },
-        ].map(({ outline, soft }) =>
-          darkBackground(
-            variant === 'secondary',
-            `<mg-tag style="margin: 0.2rem;" ${renderAttributes({ variant, outline, soft })}>${
-              icon ? `<mg-icon ${renderAttributes({ icon: 'user', size: 'small' })}></mg-icon>` : ''
-            }${variant}</mg-tag>`,
-          ),
-        ),
-      ),
-    ),
-  )('Should render %s', async (page: PageType, html: string) => {
-    await setPageContent(page, html);
+test.describe('mg-tag', () => {
+  variants.forEach(variant => {
+    test.describe(`${variant}`, () => {
+      [
+        // { icon: true, outline: true, soft: true },
+        { icon: true, outline: true, soft: false },
+        { icon: true, outline: false, soft: true },
+        { icon: true, outline: false, soft: false },
+        // { icon: false, outline: true, soft: true },
+        { icon: false, outline: true, soft: false },
+        { icon: false, outline: false, soft: true },
+        { icon: false, outline: false, soft: false },
+      ].forEach(args => {
+        test(`Should render with icon ${args.icon.toString()}, outline ${args.outline.toString()}, soft ${args.soft.toString()}`, async ({ page }) => {
+          const html = `<mg-tag ${renderAttributes({ ...args, variant })}>${
+            args.icon ? `<mg-icon ${renderAttributes({ icon: 'user', size: 'small' })}></mg-icon>` : ''
+          }${variant}</mg-tag>`;
 
-    await page.locator('mg-tag.hydrated').waitFor({ timeout: 1000 });
+          page.setContent(html);
+          page.addStyleTag({ content: '.e2e-screenshot{padding:.1rem}' });
 
-    await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+          await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+        });
+      });
+    });
   });
 
   test('Should render a 2 lines tag', async ({ page }) => {
-    await setPageContent(page, `<mg-tag>Tag with a<br> two lines text</mg-tag>`);
+    page.setContent(`<mg-tag>Tag with a<br> two lines text</mg-tag>`);
 
     await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
   });
 
   test('Should render a tag in a paragraph', async ({ page }) => {
-    await setPageContent(page, `<p>This is a <mg-tag>tag</mg-tag> in a paragraph.</p>`);
+    page.setContent(`<p>This is a <mg-tag>tag</mg-tag> in a paragraph.</p>`);
 
     await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
   });
