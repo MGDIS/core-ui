fafa57f85e21d03ed284ac916c26844e801b4a91
duhem-s <duhem-s@mgdis.fr>
2023-12-19 00:24:50 +0100

    chore(e2e): update mg-tooltip tests
    CORE-2967

--- packages/mg-components/src/components/atoms/mg-tooltip/test/mg-tooltip.e2e.ts
+++ packages/mg-components/src/components/atoms/mg-tooltip/test/mg-tooltip.e2e.ts
@@ -1,10 +1,11 @@
-import { PageType, describe, describeEach, expect, setPageContent, testEach, updateScreenshotClass, test } from '../../../../utils/playwright.e2e.test.utils';
+import { expect } from '@playwright/test';
+import { renderAttributes } from '@mgdis/playwright-helpers';
+import { test } from '../../../../utils/playwright.fixture';
 
-const TIMEOUT = 1000;
-const style = '<style>mg-tooltip{display:contents}mg-icon{position:fixed;top:50%;left:50%;transform:translate(-50%,-50%)}</style>';
+const createHTML = (args, slot = '') => `<mg-tooltip ${renderAttributes(args)}>${slot}</mg-tooltip>`;
 
-describe('mg-tooltip', () => {
-  testEach([
+test.describe('mg-tooltip', () => {
+  [
     'auto',
     'auto-start',
     'auto-end',
@@ -20,102 +21,106 @@ describe('mg-tooltip', () => {
     'left',
     'left-start',
     'left-end',
-  ])('placement %s', async (page: PageType, placement: string) => {
-    await setPageContent(page, `${style}<mg-tooltip message="this is a tooltip message" placement="${placement}"><mg-icon icon="info-circle"></mg-icon></mg-tooltip>`, {
-      width: 400,
-      height: 400,
-    });
-    await updateScreenshotClass(page, { width: '400px', height: '400px' });
+  ].forEach(placement => {
+    test(`placement ${placement}`, async ({ page }) => {
+      page.addStyleTag({ content: '.e2e-screenshot{display:block}mg-icon{position:fixed;top:50%;left:50%;transform:translate(-50%,-50%)}' });
+      await page.setViewportSize({ width: 400, height: 400 });
 
-    await page.locator('mg-tooltip.hydrated').waitFor({ timeout: TIMEOUT });
+      const html = createHTML({ placement, message: 'this is a tooltip message' }, '<mg-icon icon="info-circle"></mg-icon>');
+      await page.setContent(html);
 
-    const mgIcon = page.locator('mg-icon');
-    await mgIcon.waitFor({ timeout: TIMEOUT });
+      const mgIcon = page.locator('mg-icon');
+      await mgIcon.waitFor();
 
-    await mgIcon.focus();
+      await mgIcon.focus();
 
-    const tooltip = await page.$('mg-tooltip-content');
-
-    expect(await tooltip.getAttribute('data-show')).toEqual('');
-
-    await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
-
-    await mgIcon.blur();
-
-    expect(await tooltip.getAttribute('data-show')).toEqual(null);
-
-    await page.mouse.move(200, 200);
-
-    expect(await tooltip.getAttribute('data-show')).toEqual('');
-
-    await page.mouse.move(0, 0);
-    await page.waitForTimeout(100);
-
-    expect(await tooltip.getAttribute('data-show')).toEqual(null);
-  });
-
-  describeEach([
-    '<mg-tooltip message="my tooltip coucou"><span>coucou</span></mg-tooltip>',
-    `<mg-tooltip message="this is a tooltip message"><mg-icon icon="info-circle"></mg-icon></mg-tooltip>`,
-    '<mg-tooltip message="my tooltip mg-button"><mg-button>mg-button</mg-button></mg-tooltip>',
-    '<mg-tooltip message="my tooltip native button"><button>native button</button></mg-tooltip>',
-  ])('Keyboard navigation with template %s', (template: string) => {
-    test('Should NOT navigate with keyboard', async ({ page }) => {
-      await setPageContent(page, template);
-
-      await page.locator('mg-tooltip.hydrated').waitFor({ timeout: TIMEOUT });
-
-      await page.keyboard.down('Tab');
-
-      const tooltip = await page.$('mg-tooltip-content');
+      const tooltip = page.locator('mg-tooltip-content');
+      await tooltip.waitFor();
 
       expect(await tooltip.getAttribute('data-show')).toEqual('');
 
-      await page.keyboard.down('Enter');
+      await expect(page.locator('body')).toHaveScreenshot();
+
+      await mgIcon.blur();
+
+      expect(await tooltip.getAttribute('data-show')).toEqual(null);
+
+      await page.mouse.move(200, 200);
 
       expect(await tooltip.getAttribute('data-show')).toEqual('');
 
-      await page.keyboard.down('Space');
-
-      expect(await tooltip.getAttribute('data-show')).toEqual('');
-    });
-
-    test('Should navigate with keyboard, case key "Escape"', async ({ page }) => {
-      await setPageContent(page, template);
-
-      await page.locator('mg-tooltip.hydrated').waitFor({ timeout: TIMEOUT });
-
-      await page.keyboard.down('Tab');
-
-      const tooltip = await page.$('mg-tooltip-content');
-
-      expect(await tooltip.getAttribute('data-show')).toEqual('');
-
-      await page.keyboard.down('Escape');
+      await page.mouse.move(0, 0);
+      await page.waitForTimeout(100);
 
       expect(await tooltip.getAttribute('data-show')).toEqual(null);
     });
   });
 
-  test('Should keep tooltip displaied when hover with mouse after focus event', async ({ page }) => {
-    await setPageContent(page, '<mg-tooltip identifier="identifier" message="Batman tooltip"><mg-icon icon="user"></mg-icon></mg-tooltip>');
+  ['<span>coucou</span>', '<mg-icon icon="info-circle"></mg-icon>', '<mg-button>mg-button</mg-button>', '<button>native button</button>'].forEach(slot => {
+    test.describe(`slot ${slot}`, () => {
+      test('Should NOT navigate with keyboard', async ({ page }) => {
+        const html = createHTML({ message: 'message' }, slot);
+        await page.setContent(html);
+
+        await page.locator('mg-tooltip.hydrated').waitFor();
+
+        await page.keyboard.down('Tab');
+
+        const tooltip = page.locator('mg-tooltip-content');
+        await tooltip.waitFor();
+
+        expect(await tooltip.getAttribute('data-show')).toEqual('');
+
+        await page.keyboard.down('Enter');
+
+        expect(await tooltip.getAttribute('data-show')).toEqual('');
+
+        await page.keyboard.down('Space');
+
+        expect(await tooltip.getAttribute('data-show')).toEqual('');
+      });
+
+      test('Should navigate with keyboard, case key "Escape"', async ({ page }) => {
+        const html = createHTML({ message: 'message' }, slot);
+        await page.setContent(html);
+
+        await page.locator('mg-tooltip.hydrated').waitFor();
+
+        await page.keyboard.down('Tab');
+
+        const tooltip = page.locator('mg-tooltip-content');
+        await tooltip.waitFor();
+
+        expect(await tooltip.getAttribute('data-show')).toEqual('');
+
+        await page.keyboard.down('Escape');
+
+        expect(await tooltip.getAttribute('data-show')).toEqual(null);
+      });
+    });
+  });
+
+  test('Should keep tooltip displayed when hover with mouse after focus event', async ({ page }) => {
+    const html = createHTML({ message: 'message' }, '<mg-icon icon="user"></mg-icon>');
+    await page.setContent(html);
 
     const mgIcon = page.locator('mg-icon');
-    await mgIcon.waitFor({ timeout: TIMEOUT });
+    await mgIcon.waitFor();
 
-    const tooltip = await page.$('mg-tooltip-content');
+    const tooltip = page.locator('mg-tooltip-content');
+    await mgIcon.waitFor();
 
     // 1. take focus on mgIcon and display tooltip
     await mgIcon.focus();
 
     expect(await tooltip.getAttribute('data-show')).toEqual('');
 
-    // 2. mouseenter on mgIcon and tooltip stay displaied
+    // 2. mouseenter on mgIcon and tooltip stay displayed
     await mgIcon.hover();
 
     expect(await tooltip.getAttribute('data-show')).toEqual('');
 
-    // 3. mouseleave on mgIcon and tooltip stay displaied
+    // 3. mouseleave on mgIcon and tooltip stay displayed
     await page.mouse.move(0, 0);
 
     expect(await tooltip.getAttribute('data-show')).toEqual('');
@@ -127,63 +132,58 @@ describe('mg-tooltip', () => {
   });
 
   test('Should keep tooltip arrow when update message', async ({ page }) => {
-    await setPageContent(page, '<mg-tooltip message="Batman tooltip"><mg-icon icon="user"></mg-icon></mg-tooltip>', { width: 100, height: 50 });
-
-    await updateScreenshotClass(page, { width: '100px', height: '50px' });
+    page.addStyleTag({ content: '.e2e-screenshot{display:block}' });
+    const html = createHTML({ message: 'Batman tooltip' }, '<mg-icon icon="user"></mg-icon>');
+    await page.setContent(html);
+    await page.setViewportSize({ width: 110, height: 50 });
 
     await page.keyboard.down('Tab');
 
-    await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+    await expect(page.locator('body')).toHaveScreenshot();
 
-    await page.$eval('mg-tooltip', elm => {
-      (elm as HTMLMgTooltipElement).message = 'Joker is here';
+    await page.locator('mg-tooltip').evaluate((elm: HTMLMgTooltipElement) => {
+      elm.message = 'Joker is here';
     });
 
-    await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+    await expect(page.locator('body')).toHaveScreenshot();
   });
 
-  test(`should position tooltip where it have enough place`, async ({ page }) => {
-    await setPageContent(
-      page,
-      `<style>mg-icon{position:fixed;left:0;bottom:0}</style>
-      <mg-tooltip message="Batman tooltip" display><mg-icon icon="user"></mg-icon></mg-tooltip>`,
-      { width: 150, height: 50 },
-    );
+  test('Should position tooltip where it have enough place', async ({ page }) => {
+    page.addStyleTag({ content: '.e2e-screenshot{display:block}' });
+    const html = createHTML({ message: 'Batman tooltip', display: true }, '<mg-icon icon="user"></mg-icon>');
+    await page.setContent(html);
 
-    await updateScreenshotClass(page, { width: '150px', height: '50px' });
+    await page.addStyleTag({ content: 'mg-icon{position:fixed;left:0;bottom:0}' });
+    await page.setViewportSize({ width: 150, height: 50 });
 
-    await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+    await expect(page.locator('body')).toHaveScreenshot();
   });
 
   test('Should display long tooltip with max width', async ({ page }) => {
-    await setPageContent(
-      page,
-      '<mg-tooltip identifier="identifier" message="my very long content should return to line because of the max-width set to 400px in the design specification"><mg-icon icon="user"></mg-icon></mg-tooltip>',
-      { width: 500, height: 100 },
+    const html = createHTML(
+      { message: 'my very long content should return to line because of the max-width set to 400px in the design specification' },
+      '<mg-icon icon="user"></mg-icon>',
     );
-    await updateScreenshotClass(page, { width: '500px', height: '100px' });
+    await page.setContent(html);
+    await page.setViewportSize({ width: 500, height: 100 });
 
-    const mgIcon = page.locator('mg-icon');
-    await mgIcon.waitFor({ timeout: TIMEOUT });
+    await page.locator('mg-icon').focus();
 
-    await mgIcon.focus();
-
-    await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+    await expect(page.locator('body')).toHaveScreenshot();
   });
 
   test('Should keep tooltip center when update "message" prop', async ({ page }) => {
-    await setPageContent(page, `${style}<mg-tooltip identifier="identifier" message="short tooltip" display><mg-icon icon="user"></mg-icon></mg-tooltip>`, {
-      width: 400,
-      height: 400,
+    page.addStyleTag({ content: '.e2e-screenshot{display:block}mg-icon{position:fixed;top:50%;left:50%;transform:translate(-50%,-50%)}' });
+    const html = createHTML({ message: 'short tooltip', display: true }, '<mg-icon icon="user"></mg-icon>');
+    await page.setContent(html);
+    await page.setViewportSize({ width: 400, height: 400 });
+
+    await expect(page.locator('body')).toHaveScreenshot();
+
+    await page.locator('mg-tooltip').evaluate((elm: HTMLMgTooltipElement) => {
+      elm.message = 'my very long content should return to line because of the max-width set to 400px in the design specification';
     });
-    await updateScreenshotClass(page, { width: '400px', height: '400px' });
 
-    await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
-
-    await page.$eval('mg-tooltip', (mgTooltip: HTMLMgTooltipElement) => {
-      mgTooltip.message = 'my very long content should return to line because of the max-width set to 400px in the design specification';
-    });
-
-    await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+    await expect(page.locator('body')).toHaveScreenshot();
   });
 });
