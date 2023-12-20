38a203a79678f33d7cdffa4c35bd7701c2c45917
jouanno-n <jouanno-n@mgdis.fr>
2023-12-20 10:54:25 +0100

    feat(mgInputDate): display default help text with pattern
    CORE-2509

--- packages/mg-components/src/utils/locale.utils.ts
+++ packages/mg-components/src/utils/locale.utils.ts
@@ -25,6 +25,25 @@ export const localeNumber = (number: number, locale: string): string => {
 export const dateRegExp = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
 
 /**
+ * Get date pattern base on locale
+ * @param locale to tefer
+ * @returns date pattern
+ */
+export const getLocaleDatePattern = (locale: string) => {
+  const year = { value: '2023', pattern: 'YYYY' };
+  const month = { value: '12', pattern: 'MM' };
+  const day = { value: '24', pattern: 'DD' };
+  return localeDate([year.value, month.value, day.value].join('-'), locale).replace(year.value, year.pattern).replace(month.value, month.pattern).replace(day.value, day.pattern);
+};
+
+/**
+ * Format a date object to string
+ * @param date to parse
+ * @returns string date with pattern 'YYYY-MM-DD'
+ */
+export const dateToString = (date: Date): string => date.toISOString().split('T')[0];
+
+/**
  * Locale date format
  * @param date - date to format
  * @param locale - locale to apply
