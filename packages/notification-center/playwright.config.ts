2eab3b29e9c92e051014ab8263d8677c3135c55d
duhem-s <duhem-s@mgdis.fr>
2024-01-23 16:43:41 +0100

    chore(notifCenter): increase timeout

--- packages/notification-center/playwright.config.ts
+++ packages/notification-center/playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   ...playwrightBaseConfig,
+  timeout: 60 * 1000, // increased timeout because of waiting times in tests
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'cd ../.. && pnpm apps:notification-center',
