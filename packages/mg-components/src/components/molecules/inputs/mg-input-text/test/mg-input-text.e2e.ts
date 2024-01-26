97d6aabe62d646af49754673d4ca9dca742d461b
duhem-s <duhem-s@mgdis.fr>
2024-01-26 14:43:01 +0100

    feat(mgComp/mgInputs): responsive
    CORE-3052

--- packages/mg-components/src/components/molecules/inputs/mg-input-text/test/mg-input-text.e2e.ts
+++ packages/mg-components/src/components/molecules/inputs/mg-input-text/test/mg-input-text.e2e.ts
@@ -140,9 +140,7 @@ describe('mg-input-text', () => {
       const element = page.locator('mg-input-text.hydrated');
       await element.waitFor({ timeout: 1000 });
 
-      await page.setViewportSize({ height: 100, width: 500 });
-
-      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+      await expect(page).toHaveScreenshot({ clip: { x: 0, y: 0, height: 100, width: 500 } });
     });
 
     describeEach([true, false])('using append-input slot, case readonly %s', (readonly: boolean) => {
