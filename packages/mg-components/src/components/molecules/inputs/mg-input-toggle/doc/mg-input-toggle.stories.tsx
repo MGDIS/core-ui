e830cd661ef149f751ff115b773f9b464be452ea
jouanno-n <jouanno-n@mgdis.fr>
2024-02-14 09:21:38 +0100

    feat(MgInput): retreive mg-input-toggle style
    CORE-2910

--- packages/mg-components/src/components/molecules/inputs/mg-input-toggle/doc/mg-input-toggle.stories.tsx
+++ packages/mg-components/src/components/molecules/inputs/mg-input-toggle/doc/mg-input-toggle.stories.tsx
@@ -26,7 +26,7 @@ const args = {
   identifier: 'identifier',
   name: 'input-name',
   // Label
-  label: 'Option',
+  label: 'Label',
   labelOnTop: false,
   labelHide: false,
   // toggle
@@ -65,7 +65,7 @@ export const MgInputToggle = {
  * @returns HTMLElement
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const TemplateIcon = (args: MgInputToggleType): HTMLElement => (
+const TemplateIcon = (args: MgInputToggleType): HTMLMgInputToggleElement => (
   <mg-input-toggle {...filterArgs(args)}>
     <span slot="item-1">
       <mg-icon icon="cross"></mg-icon>
