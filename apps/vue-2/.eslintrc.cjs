41cc9e6a45912bf0cc041949266791b410fd7328
duhem-s <duhem-s@mgdis.fr>
2024-02-23 16:48:48 +0100

    fix(apps): allow redondant main role
    CORE-3107

--- apps/vue-2/.eslintrc.cjs
+++ apps/vue-2/.eslintrc.cjs
@@ -3,4 +3,13 @@ const { eslintrc } = require('@mgdis/linting-vue2');
 module.exports = {
   root: true,
   ...eslintrc,
+  rules: {
+    ...eslintrc.rules,
+    'vuejs-accessibility/no-redundant-roles': [
+      'error',
+      {
+        main: ['main'],
+      },
+    ],
+  },
 };
