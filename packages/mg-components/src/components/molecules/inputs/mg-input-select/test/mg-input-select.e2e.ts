ebf6a4d9066d4f6af39e5c44bc2afbface32b878
duhem-s <duhem-s@mgdis.fr>
2024-03-21 16:21:37 +0100

    test(mgComp/mgInputSelect): update PW tests
    CORE-3205

--- packages/mg-components/src/components/molecules/inputs/mg-input-select/test/mg-input-select.e2e.ts
+++ packages/mg-components/src/components/molecules/inputs/mg-input-select/test/mg-input-select.e2e.ts
@@ -1,147 +1,154 @@
-import { renderAttributes } from '@mgdis/playwright-helpers';
-import { PageType, describe, describeEach, expect, setPageContent, testEach, test } from '../../../../../utils/playwright.e2e.test.utils';
-
-const TIMEOUT = 1000;
+import { expect } from '@playwright/test';
+import { test } from '../../../../../utils/playwright.fixture';
+import { renderAttributes, renderProperties } from '@mgdis/playwright-helpers';
 import { widths } from '../../mg-input/mg-input.conf';
 
-describe('mg-input-select', () => {
-  describeEach([true, false])('without tooltip, case label on top %s', labelOnTop => {
-    test('render', async ({ page }) => {
-      await setPageContent(
-        page,
-        `
-      <mg-input-select ${renderAttributes({ identifier: 'identifier', label: 'label', labelOnTop })}></mg-input-select>
-      <script>
-      const mgInputSelect = document.querySelector('mg-input-select');
-      mgInputSelect.items = ['blu', 'bli', 'bla', 'blo'];
-      </script>`,
-      );
+const baseArgs = {
+  identifier: 'identifier',
+  label: 'label',
+  items: ['blu', 'bli', 'bla', 'blo'],
+};
 
-      await page.locator('mg-input-select.hydrated').waitFor({ timeout: TIMEOUT });
+const createHTML = args => `<mg-input-select ${renderAttributes(args)}></mg-input-select>`;
 
-      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+test.describe('mg-input-select', () => {
+  [true, false].forEach(labelOnTop => {
+    test.describe(`labelOnTop: ${labelOnTop}`, () => {
+      test(`Should render with template`, async ({ page }) => {
+        const componentArgs = { ...baseArgs, labelOnTop };
+        const html = createHTML(componentArgs);
+        console.log(html);
 
-      await page.keyboard.down('Tab');
+        await page.setContent(html);
+        await page.addScriptTag({ content: renderProperties(componentArgs, `[identifier="${componentArgs.identifier}"]`) });
 
-      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+        await page.waitForSelector('mg-input-select.hydrated');
 
-      await page.keyboard.down('Space');
+        await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
 
-      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+        await page.keyboard.down('Tab');
 
-      await page.keyboard.down('ArrowDown');
-      await page.keyboard.down('ArrowDown');
+        await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
 
-      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+        await page.keyboard.down('Space');
 
-      await page.keyboard.down('Enter');
+        await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
 
-      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
-    });
+        await page.keyboard.down('ArrowDown');
+        await page.keyboard.down('ArrowDown');
 
-    testEach([undefined, ...widths])('render with width %s', async (page: PageType, mgWidth) => {
-      await setPageContent(
-        page,
-        `
-      <mg-input-select ${renderAttributes({ identifier: 'identifier', label: 'label', mgWidth, labelOnTop })}></mg-input-select>
-      <script>
-      const mgInputSelect = document.querySelector('mg-input-select');
-      mgInputSelect.items = ['blu', 'bli', 'bla', 'blo'];
-      </script>`,
-      );
+        await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
 
-      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+        await page.keyboard.down('Enter');
+
+        await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+      });
+
+      [undefined, ...widths].forEach(mgWidth => {
+        test(`render with width=${mgWidth}`, async ({ page }) => {
+          const componentArgs = { ...baseArgs, labelOnTop, mgWidth };
+          const html = createHTML(componentArgs);
+          await page.setContent(html);
+          await page.addScriptTag({ content: renderProperties(componentArgs, `[identifier="${componentArgs.identifier}"]`) });
+
+          await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+        });
+      });
+
+      test('render with tooltip', async ({ page }) => {
+        const componentArgs = { ...baseArgs, labelOnTop, tooltip: 'Tooltip message' };
+        const html = createHTML(componentArgs);
+        await page.setContent(html);
+        await page.addScriptTag({ content: renderProperties(componentArgs, `[identifier="${componentArgs.identifier}"]`) });
+
+        await page.waitForSelector('mg-input-select.hydrated');
+
+        await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+
+        await page.keyboard.down('Tab');
+        if (!labelOnTop) {
+          await page.keyboard.down('Tab');
+        }
+
+        await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+      });
+
+      test(`render inside a div.mg-form-group`, async ({ page }) => {
+        const componentArgs = {
+          ...baseArgs,
+          tooltip: 'Tooltip message',
+          labelOnTop,
+          label: 'long label long label long label long label long label long label long label long label long label long label long label',
+        };
+        const html = createHTML(componentArgs);
+        await page.setContent(`<div class="mg-form-group">${html}</div>`);
+        await page.addScriptTag({ content: renderProperties(componentArgs, `[identifier="${componentArgs.identifier}"]`) });
+
+        await page.waitForSelector('mg-input-select.hydrated');
+
+        await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+      });
+
+      test('Ensure component fit in width 300px', async ({ page }) => {
+        const componentArgs = {
+          ...baseArgs,
+          labelOnTop,
+          items: ['blu', 'bli', 'bla', 'blo', 'le long libellé qui va faire sortir le champ mg-input-select de sa zone de confort'],
+        };
+        const html = createHTML(componentArgs);
+        await page.setContent(html);
+        await page.addScriptTag({ content: renderProperties(componentArgs, `[identifier="${componentArgs.identifier}"]`) });
+
+        await page.setViewportSize({ width: 300, height: 100 });
+
+        await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+      });
     });
   });
 
-  describeEach([
-    `<mg-input-select identifier="identifier" label="label" label-on-top></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" label-hide></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" placeholder="placeholder" help-text="HelpText Message"></mg-input-select>`,
-  ])('without tooltip', (html: string) => {
-    test(`render ${html}`, async ({ page }) => {
-      await setPageContent(
-        page,
-        `${html}
-        <script>
-        const mgInputSelect = document.querySelector('mg-input-select');
-        mgInputSelect.items = ['blu', 'bli', 'bla', 'blo'];
-        </script>`,
-      );
-
-      await page.locator('mg-input-select.hydrated').waitFor({ timeout: TIMEOUT });
-
-      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
-    });
-  });
-
-  testEach([true, false])('render with tooltip, case label-on-top %s', async (page: PageType, labelOnTop: boolean) => {
-    await setPageContent(
-      page,
-      `<mg-input-select identifier="identifier" label="label" tooltip="Tooltip message" label-on-top="${labelOnTop}"></mg-input-select>
-      <script>
-      const mgInputSelect = document.querySelector('mg-input-select');
-      mgInputSelect.items = ['blu', 'bli', 'bla', 'blo'];
-      </script>`,
-    );
-
-    await page.locator('mg-input-select.hydrated').waitFor({ timeout: TIMEOUT });
-
-    await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
-
-    await page.keyboard.down('Tab');
-    if (!labelOnTop) {
-      await page.keyboard.down('Tab');
-    }
-
-    await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
-  });
-
-  describeEach([
-    `<mg-input-select identifier="identifier" label="label" readonly></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" value="blu"></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" value="blu" readonly></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" value="blu" readonly label-on-top></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" disabled></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" value="blu" disabled></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" value="batman" help-text='<mg-icon icon="user" size="small"></mg-icon> Welcome batman'></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" value="blu" required help-text="HelpText Message"></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" value="blu" required readonly help-text="HelpText Message"></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" value="blu" required disabled help-text="HelpText Message"></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" value="blu" tooltip="blu" tooltip-position="label"></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" value="blu" tooltip="blu" tooltip-position="input" label-on-top></mg-input-select>`,
-  ])('Should render with template', (html: string) => {
-    test(`render ${html}`, async ({ page }) => {
-      await setPageContent(
-        page,
-        `${html}
-        <script>
-        const mgInputSelect = document.querySelector('mg-input-select');
-        mgInputSelect.items = ['blu', 'bli', 'bla', 'blo'];
-        </script>`,
-      );
-
-      await page.locator('mg-input-select.hydrated').waitFor({ timeout: TIMEOUT });
+  [{ labelHide: true }, { placeholder: 'placeholder', helpText: 'HelpText Message' }].forEach(args => {
+    test(`Should render with template ${renderAttributes(args)}`, async ({ page }) => {
+      const componentArgs = { ...baseArgs, ...args };
+      const html = createHTML(componentArgs);
+      await page.setContent(html);
+      await page.addScriptTag({ content: renderProperties(componentArgs, `[identifier="${componentArgs.identifier}"]`) });
 
       await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
     });
   });
 
-  describeEach([
-    `<mg-input-select identifier="identifier" label="label" required></mg-input-select>`,
-    `<mg-input-select identifier="identifier" label="label" required lang="fr"></mg-input-select>`,
-  ])('%s', (html: string) => {
-    test('Should render error when leaving an empty required input', async ({ page }) => {
-      await setPageContent(
-        page,
-        `${html}
-      <script>
-      const mgInputSelect = document.querySelector('mg-input-select');
-      mgInputSelect.items = ['blu', 'bli', 'bla', 'blo'];
-      </script>`,
-      );
+  [
+    { readonly: true },
+    { value: 'blu' },
+    { value: 'blu', readonly: true },
+    { value: 'blu', readonly: true, labelOnTop: true },
+    { disabled: true },
+    { value: 'blu', disabled: true },
+    { value: 'batman', helpText: '<mg-icon icon="user" size="small"></mg-icon> Welcome batman' },
+    { value: 'blu', required: true, helpText: 'HelpText Message' },
+    { value: 'blu', required: true, readonly: true, helpText: 'HelpText Message' },
+    { value: 'blu', required: true, disabled: true, helpText: 'HelpText Message' },
+    { value: 'blu', tooltip: 'blu', tooltipPosition: 'label' },
+    { value: 'blu', tooltip: 'blu', tooltipPosition: 'input', labelOnTop: true },
+  ].forEach(args => {
+    test(`Should render with template ${renderAttributes(args)}`, async ({ page }) => {
+      const componentArgs = { ...baseArgs, ...args };
+      const html = createHTML(componentArgs);
+      await page.setContent(html);
+      await page.addScriptTag({ content: renderProperties(componentArgs, `[identifier="${componentArgs.identifier}"]`) });
 
-      await page.locator('mg-input-select.hydrated').waitFor({ timeout: TIMEOUT });
+      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
+    });
+  });
+
+  [{}, { lang: 'fr' }].forEach(args => {
+    test(`Should render error when leaving an empty input ${renderAttributes(args)}`, async ({ page }) => {
+      const componentArgs = { ...baseArgs, ...args, required: true };
+      const html = createHTML(componentArgs);
+      await page.setContent(html);
+      await page.addScriptTag({ content: renderProperties(componentArgs, `[identifier="${componentArgs.identifier}"]`) });
+
+      await page.waitForSelector('mg-input-select.hydrated');
 
       await page.keyboard.down('Tab');
       await page.keyboard.down('Tab');
@@ -151,23 +158,22 @@ describe('mg-input-select', () => {
   });
 
   test('Should render a grouped list', async ({ page }) => {
-    await setPageContent(
-      page,
-      `<mg-input-select identifier="identifier" label="label"></mg-input-select>
-      <script>
-      const mgInputSelect = document.querySelector('mg-input-select');
-      mgInputSelect.items = [
+    const componentArgs = {
+      ...baseArgs,
+      items: [
         { title: 'blu', value: 'blu', group: 'Le groupe A' },
         { title: 'blu', value: 'blublu', group: 'Le groupe B' },
         { title: 'bli', value: 'bli', group: 'Le groupe A' },
         { title: 'bli', value: 'blibli', group: 'Le groupe B' },
         { title: 'bla', value: 'blabla' },
         { title: 'blo', value: 'bloblo' },
-      ];
-      </script>`,
-    );
+      ],
+    };
+    const html = createHTML(componentArgs);
+    await page.setContent(html);
+    await page.addScriptTag({ content: renderProperties(componentArgs, `[identifier="${componentArgs.identifier}"]`) });
 
-    await page.locator('mg-input-select.hydrated').waitFor({ timeout: TIMEOUT });
+    await page.waitForSelector('mg-input-select.hydrated');
 
     await page.keyboard.down('Tab');
     await page.keyboard.down('Space');
@@ -175,75 +181,13 @@ describe('mg-input-select', () => {
     await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
   });
 
-  describeEach([
-    '<mg-input-select identifier="identifier" label="long label long label long label long label long label long label long label long label long label long label long label" tooltip="tooltip message"></mg-input-select>',
-    '<mg-input-select identifier="identifier" label="long label long label long label long label long label long label long label long label long label long label long label" tooltip="tooltip message" label-on-top></mg-input-select>',
-  ])('inside a div.mg-form-group', (html: string) => {
-    test(`render ${html}`, async ({ page }) => {
-      await setPageContent(
-        page,
-        `<div class="mg-form-group">${html}</div>
-      <script>
-      const mgInputSelect = document.querySelector('mg-input-select');
-      mgInputSelect.items = ['blu', 'bli', 'bla', 'blo'];
-      </script>`,
-      );
-
-      await page.locator('mg-input-select.hydrated').waitFor({ timeout: TIMEOUT });
-
-      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
-    });
-  });
-
-  describeEach(['full', 16])('with custom width: %s', width => {
-    testEach([false, true])('with label on top: %s', async (page: PageType, labelOnTop: boolean) => {
-      await setPageContent(
-        page,
-        `
-        <mg-input-select identifier="identifier" label="label" mg-width="${width}" label-on-top="${labelOnTop}"></mg-input-select>
-        <script>
-          const mgInputSelect = document.querySelector('mg-input-select');
-          mgInputSelect.items = ['blu', 'bli', 'bla', 'blo'];
-        </script>
-      `,
-      );
-
-      await page.locator('mg-input-select.hydrated').waitFor({ timeout: TIMEOUT });
-
-      await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
-    });
-  });
-
-  testEach([false, true])('Ensure component fit in width 300px with label-on-top: %s', async (page: PageType, labelOnTop: boolean) => {
-    await setPageContent(
-      page,
-      `
-      <mg-input-select identifier="identifier" label="label" label-on-top="${labelOnTop}"></mg-input-select>
-      <script>
-        const mgInputSelect = document.querySelector('mg-input-select');
-        mgInputSelect.items = ['blu', 'bli', 'bla', 'blo', 'le long libellé qui va faire sortir le champ mg-input-select de sa zone de confort'];
-      </script>
-    `,
-    );
-
-    await page.locator('mg-input-select.hydrated').waitFor({ timeout: TIMEOUT });
-
-    await page.setViewportSize({ width: 300, height: 100 });
-
-    await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
-  });
-
   test.describe('Responsive', () => {
     [{}, { tooltip: 'blu' }, { tooltip: 'blu', tooltipPosition: 'label' }].forEach(args => {
       test(`Should display label on top on responsive breakpoint with tooltip message: ${renderAttributes(args)}`, async ({ page }) => {
-        await setPageContent(
-          page,
-          `<mg-input-select identifier="identifier" label="label" ${renderAttributes(args)}></mg-input-select>
-          <script>
-          const mgInputSelect = document.querySelector('mg-input-select');
-          mgInputSelect.items = ['blu', 'bli', 'bla', 'blo', 'le long libellé qui va faire sortir le champ mg-input-select de sa zone de confort'];
-        </script>`,
-        );
+        const componentArgs = { ...baseArgs, ...args, items: ['blu', 'bli', 'bla', 'blo', 'le long libellé qui va faire sortir le champ mg-input-select de sa zone de confort'] };
+        const html = createHTML(componentArgs);
+        await page.setContent(html);
+        await page.addScriptTag({ content: renderProperties(componentArgs, `[identifier="${componentArgs.identifier}"]`) });
 
         // Initial state
         await expect(page.locator('.e2e-screenshot')).toHaveScreenshot();
