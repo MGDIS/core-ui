2dbfe477f2ff6ed9c7bd14db2dd9085ab49f058e
duhem-s <duhem-s@mgdis.fr>
2025-03-05 10:49:38 +0100

    chore(mg-components): update stories
    CORE-3057

--- packages/mg-components/src/components/molecules/mg-table/docs/mg-table.stories.tsx
+++ packages/mg-components/src/components/molecules/mg-table/docs/mg-table.stories.tsx
@@ -1,18 +1,11 @@
 import { h } from '@stencil/core';
 import { filterArgs } from '@mgdis/stencil-helpers';
 import type { MgTable as MgTableType } from '../mg-table';
-import { alignments } from '../mg-table.conf';
 
 export default {
   component: 'mg-table',
   title: 'Molecules/mg-table',
   tags: ['beta'],
-  argTypes: {
-    columnsAlignment: {
-      options: [undefined, ...alignments],
-      control: { type: 'select' },
-    },
-  },
 };
 
 /**
@@ -25,7 +18,9 @@ const Template = (args: MgTableType & { slotContent: string }): HTMLElement => (
     <table>
       <thead>
         <tr>
-          <th>Dev</th>
+          <th>Name</th>
+          <th>Birthday</th>
+          <th>Age</th>
           <th>Role</th>
           <th>Test signature</th>
         </tr>
@@ -33,16 +28,22 @@ const Template = (args: MgTableType & { slotContent: string }): HTMLElement => (
       <tbody>
         <tr>
           <th>Simon</th>
-          <td>Archi front-end</td>
+          <td data-sort="1982-06-02">02/06/1982</td>
+          <td>42</td>
+          <td>Archi</td>
           <td>Blu / Daron Crew</td>
         </tr>
         <tr>
           <th>Nico</th>
+          <td data-sort="1990-10-31">31/10/1990</td>
+          <td>35</td>
           <td>Dev front-end</td>
           <td>DC Comics (Batman, Jocker, etc.)</td>
         </tr>
         <tr>
           <th>Guirec</th>
+          <td data-sort="1985-12-30">30/12/1985</td>
+          <td>39</td>
           <td>Dev front-end</td>
           <td>Pat'Patrouille</td>
         </tr>
@@ -55,5 +56,6 @@ export const MGTable = {
   render: Template,
   args: {
     fullWidth: false,
+    columns: { 1: { sortable: true }, 2: { datatype: 'date', sortable: true }, 3: { datatype: 'numeric', sortable: true } },
   },
 };
--- packages/mg-components/src/components/molecules/mg-table/readme.md
+++ packages/mg-components/src/components/molecules/mg-table/readme.md
@@ -1,4 +1,27 @@
+## Sorting
 
+The `mg-table` component supports sorting for different data types. You can specify the sortable columns and their data types using the `columns` property.
+
+### Date
+
+To enable sorting for date columns, set the `datatype` property to `"date"` in the `columns` object.
+The date values should be in a format that can be parsed by JavaScript's `Date` object. If you need to display a different format, you must use the `data-sort` attribute on the `tr` or `th` cell, like `<td data-sort="1982-06-02">02/06/1982</td>`.
+
+### Numeric
+
+To enable sorting for numeric columns, set the `datatype` property to `"numeric"` in the `columns` object. This will ensure that the values are sorted numerically.
+Note that when defining a column as `"numeric"`, its content will be right-aligned.
+
+#### Currency
+
+For currency values, you can also use the `datatype` property set to `"numeric"` combined with the use of the `data-sort` attribute on the cell, like `<td data-sort="1234567890.12">1 234 567 890,12 €</td>`.
+
+## Ressources
+
+- [Styling Tables the Modern CSS Way - Piccalilli](https://piccalil.li/blog/styling-tables-the-modern-css-way/)
+- [Under-Engineered Responsive Tables — Adrian Roselli](https://adrianroselli.com/2020/11/under-engineered-responsive-tables.html)
+- [Sortable Table Example | APG | WAI | W3C](https://www.w3.org/WAI/ARIA/apg/patterns/table/examples/sortable-table/)
+- [aria-sort - Accessibility | MDN](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-sort)
 
 <!-- Auto Generated Below -->
 
