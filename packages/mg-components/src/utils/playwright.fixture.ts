4d1d89a991947b348b0160408dfa84481be27268
duhem-s <duhem-s@mgdis.fr>
2025-03-19 16:07:59 +0100

    chore: do not wait for hydrated class

--- packages/mg-components/src/utils/playwright.fixture.ts
+++ packages/mg-components/src/utils/playwright.fixture.ts
@@ -10,14 +10,13 @@ export const test = base.extend<{ page: void }>({
       }, html);
     };
     // Add lib JS
-    await page.addScriptTag({ url: 'http://localhost:8080/mg-components/dist/mg-components//mg-components.esm.js', type: 'module' });
+    await page.addScriptTag({ url: 'http://localhost:8080/mg-components/dist/mg-components/mg-components.esm.js', type: 'module' });
     // Add lib CSS
-    await page.addStyleTag({ url: 'http://localhost:8080/mg-components/dist/mg-components//mg-components.css' });
+    await page.addStyleTag({ url: 'http://localhost:8080/mg-components/dist/mg-components/mg-components.css' });
     // Add screenshot CSS
     await page.addStyleTag({ content: 'body{margin:0;--mg-b-font-family:"Ubuntu",sans-serif}.e2e-screenshot{display:inline-block}' });
     // Make sure everything is loaded
     await page.waitForLoadState('networkidle');
-    await page.locator('html.hydrated').waitFor({ state: 'attached' });
     // Use extended page
     await use(page);
   },
