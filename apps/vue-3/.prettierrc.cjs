7d6a41d6c937e55216f50c3a421aaed71a571210
duhem-s <duhem-s@mgdis.fr>
2025-03-20 08:40:40 +0100

    chore: remove core prettier import

--- apps/vue-3/.prettierrc.cjs
+++ apps/vue-3/.prettierrc.cjs
@@ -1,2 +1,18 @@
-const prettierConfig = require('@mgdis/prettier-config');
-module.exports = { ...prettierConfig };
+/**
+ * Shoulbe be the following code
+ *
+ * const prettierConfig = require('@mgdis/prettier-config');
+ * module.exports = { ...prettierConfig };
+ *
+ * But to prevent error build on our public GitHub repository on in renovate
+ * we will use the following code which is the same as what we import
+ */
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+};
