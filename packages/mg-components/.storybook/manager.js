dd9a1b70306ecac7dfc4c3f8e19e38ab081213ea
duhem-s <duhem-s@mgdis.fr>
2025-04-01 15:38:52 +0200

    feat(img): add mgdis logo
    CORE-3153

--- packages/mg-components/.storybook/manager.js
+++ packages/mg-components/.storybook/manager.js
@@ -1,6 +1,6 @@
 import { addons } from '@storybook/manager-api';
 import { create } from '@storybook/theming';
-import brandImage from './logo.png';
+import brandImage from '@mgdis/img/dist/logos/mgdis.svg';
 
 addons.setConfig({
   theme: create({
