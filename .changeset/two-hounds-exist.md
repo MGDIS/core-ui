1ff1809eaeebb75b1a815f4ae1937d7933da0c43
jouanno-n <jouanno-n@mgdis.fr>
2025-04-11 14:02:29 +0200

    feat(mg-action-more):  set fullwidth on <mg-divider>
    CORE-3604

---
+++ .changeset/two-hounds-exist.md
@@ -0,0 +1,5 @@
+---
+'@mgdis/mg-components': patch
+---
+
+`**<mg-action-more>**` set fullwidth on `<mg-divider>`
--- packages/mg-components/src/components/molecules/mg-action-more/mg-action-more.tsx
+++ packages/mg-components/src/components/molecules/mg-action-more/mg-action-more.tsx
@@ -208,7 +208,7 @@ export class MgActionMore {
               <mg-menu direction={directions.VERTICAL} label={this.messages.label}>
                 {this.items.map(item =>
                   isMgActionMoreDivider(item) ? (
-                    <mg-divider class="mg-c-action-more__divider" key="divider"></mg-divider>
+                    <mg-divider class="mg-c-action-more__divider" key="divider" full-width></mg-divider>
                   ) : (
                     <mg-menu-item
                       key={item.label}
