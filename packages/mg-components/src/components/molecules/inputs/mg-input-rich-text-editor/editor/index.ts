8b98093fde5e797e34d70e360dbd7c9bbd989af7
Guirec Lefort <lefort-g@mgdis.fr>
2026-01-08 18:53:40 +0100

    feat(mg-input-rich-text-editor): group jodit imports
    CORE-3640

--- packages/mg-components/src/components/molecules/inputs/mg-input-rich-text-editor/editor/index.ts
+++ packages/mg-components/src/components/molecules/inputs/mg-input-rich-text-editor/editor/index.ts
@@ -1,7 +1,18 @@
 import { isValidString } from '@mgdis/core-ui-helpers/dist/utils';
 import { Jodit } from 'jodit';
 // Import Jodit plugins
-import './jodit-plugins';
+import 'jodit/esm/plugins/select/select.js';
+import 'jodit/esm/plugins/resizer/resizer.js';
+import 'jodit/esm/plugins/table/table.js';
+import 'jodit/esm/plugins/select-cells/select-cells.js';
+import 'jodit/esm/plugins/resize-cells/resize-cells.js';
+import 'jodit/esm/plugins/table-keyboard-navigation/table-keyboard-navigation.js';
+import 'jodit/esm/plugins/inline-popup/inline-popup.js';
+import 'jodit/esm/plugins/clean-html/clean-html.js';
+import 'jodit/esm/plugins/delete/delete.js';
+import 'jodit/esm/plugins/file/file.js';
+import 'jodit/esm/plugins/print/print.js';
+import 'jodit/esm/plugins/source/source.js';
 // Import Jodit types
 import type { IJodit, ButtonsOption } from 'jodit/esm/types';
 
