7e4f0e1fe68b9d3f3b9e1e17a46d3fa1e0f7b388
Guirec Lefort <lefort-g@mgdis.fr>
2026-06-01 18:34:05 +0200

    style(mg-message): condition shadow prop on variantStyle instead of CSS override
    CORE-3794

--- packages/mg-components/src/components/molecules/mg-message/mg-message.tsx
+++ packages/mg-components/src/components/molecules/mg-message/mg-message.tsx
@@ -122,7 +122,7 @@ export class MgMessage {
   render(): HTMLElement {
     return (
       <div class={this.classCollection.join()}>
-        <mg-card shadow>
+        <mg-card shadow={this.variantStyle !== 'background'}>
           <span class="mg-c-message__icon">
             <mg-icon icon={this.getIcon()}></mg-icon>
           </span>
